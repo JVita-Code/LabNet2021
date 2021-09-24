288d30369933040442e62fee965a0f3e9b6fade1
JVita-Code <juanvitan@gmail.com>
2021-09-24 10:32:27 -0300

    Algunas logic y appconfig
    -BaseLogic
    -IABMLogic

---
+++ LabNet2021.Tp5.LINQ/LabNet2021.TP5.LINQ.Logic/BaseLogic.cs
@@ -0,0 +1,20 @@
+﻿using LabNet2021.Tp5.LINQ.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabNet2021.TP5.LINQ.Logic
+{
+    class BaseLogic
+    {
+        protected readonly NorthwindContext context;
+
+        public BaseLogic()
+        {
+            context = new NorthwindContext();
+        }
+
+    }
+}
---
+++ LabNet2021.Tp5.LINQ/LabNet2021.TP5.LINQ.Logic/IABMLogic.cs
@@ -0,0 +1,17 @@
+﻿using LabNet2021.Tp5.LINQ.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabNet2021.TP5.LINQ.Logic
+{
+    interface IABMLogic<T>
+    {
+        List<T> GetAll();
+        void Add(T newRegion);
+        void Update(T region);
+        void Delete(int id);
+    }
+}
