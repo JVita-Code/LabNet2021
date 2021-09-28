9d8f9be30f1155f9d5b530eb754accee1cd7d108
JVita-Code <juanvitan@gmail.com>
2021-09-28 18:34:52 -0300

    Insert y Delete de shipper
    - Error en insert: nuestro input solo da companyname y tenemos Phone e ID en required

--- LabNet2021.Tp7.MVC/LabNet2021.Tp7.MVC/Controllers/ShipperController.cs
+++ LabNet2021.Tp7.MVC/LabNet2021.Tp7.MVC/Controllers/ShipperController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using LabNet2021.Tp4.EF.Entities;
+using LabNet2021.Tp4.EF.Logic;
+using LabNet2021.Tp7.MVC.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +11,47 @@ namespace LabNet2021.Tp7.MVC.Controllers
 {
     public class ShipperController : Controller
     {
+        ShipperLogic logic = new ShipperLogic();
         // GET: Shipper
         public ActionResult Index()
         {
+            var logic = new ShipperLogic();
+            List<Shipper> shippers = logic.GetAll();
+
+            List<ShipperView> shippersView = shippers.Select(s => new ShipperView
+            {
+                Id = s.ShipperID,
+                CompanyName = s.CompanyName,
+                Phone = s.Phone
+
+            }).ToList();
+            return View(shippersView);
+        }
+
+        public ActionResult Insert()
+        {
             return View();
+        }
+        [HttpPost]
+        public ActionResult Insert(ShipperView shipperView)
+        {
+            try
+            {
+                Shipper shipperEntity = new Shipper { CompanyName = shipperView.CompanyName };
+
+                logic.Add(shipperEntity);
+
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                return RedirectToAction("Index", "Error");
+            }
+        }
+        public ActionResult Delete(int id)
+        {
+            logic.Delete(id);
+            return RedirectToAction("Index");
         }
     }
 }
